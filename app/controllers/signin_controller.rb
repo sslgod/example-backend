06872f8802e95995ce32cc1038a1492d31dd1146
Dmitry Silaev <dmsilaev@yandex.ru>
2020-08-06 09:59:26 +0300

    fix

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -10,7 +10,7 @@ class SigninController < ApplicationController
       response.set_cookie(JWTSessions.access_cookie,
                         value: tokens[:access],
                         httponly: true,
-                        secure: Rails.env.production?)
+                        secure: false)
       render json: { csrf: tokens[:csrf] }
     else
       not_authorized
